9f008acdaaba94a8f940bb5615756fcd4593fda3
Markus Bielaszka <69858432+Markus-55@users.noreply.github.com>
2024-02-21 00:08:13 +0100

    Chore: refactor typescript code

--- src/connectWeb3.ts
+++ src/connectWeb3.ts
@@ -1,13 +1,17 @@
 const connectButton: HTMLButtonElement | null = document.querySelector('#connect');
 
 async function connect(): Promise<void> {
-  if(typeof window.ethereum !== undefined) {
-    console.log('Connected to ethereum');
-    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
-    console.log(accounts);
-  } else {
-    console.log('you must have metamask to connect');
+  try {
+    if(typeof window.ethereum !== undefined) {
+      console.log('Connected to ethereum');
+      const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+      console.log(accounts);
+    } else {
+      console.log('you must have metamask to connect');
+    }
+  } catch(err) {
+    console.log(err);
   }
 }
 
-if(connectButton) connectButton.addEventListener('click', connect);
+connectButton?.addEventListener('click', connect);
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,51 @@
 const web3 = new Web3(window.ethereum);
 
-const getBalanceInput: HTMLInputElement | null = document.querySelector('#ethAddress');
+interface Transaction {
+  from: string;
+  to: string;
+  value: number;
+  gas: string;
+}
+
+const ethAddress: HTMLInputElement | null = document.querySelector('#ethAddress');
 const getBalanceBtn: HTMLButtonElement | null = document.querySelector('#getBalanceBtn');
 const displayBalance: HTMLParagraphElement | null = document.querySelector('#displayBalance');
 
-const valueInput: HTMLInputElement | null = document.querySelector('#value');
 const fromAddressInput: HTMLInputElement | null = document.querySelector('#fromAddress');
 const toAddressInput: HTMLInputElement | null = document.querySelector('#toAddress');
+const valueInput: HTMLInputElement | null = document.querySelector('#value');
+
 const sendTransactionBtn: HTMLButtonElement | null = document.querySelector('#sendEth');
 const displayBlocks: HTMLParagraphElement | null = document.querySelector('#displayBlocks');
-const transactionList: HTMLParagraphElement | null = document.querySelector('#transactions');
 
 function initApp() {
   console.log(web3);
 }
 
-async function getBalance() {
-  const accountBalance = getBalanceInput ? getBalanceInput.value : '';
+async function getBalance(account: string): Promise<number> {
+  const balance: number = await web3.eth.getBalance(account);
+
+  try {
+    if(displayBalance) displayBalance.innerHTML = `${web3.utils.fromWei(balance, 'ether')} ETH`;
+  } catch(err) {
+    console.log(err);
+  }
   
-  const balance = await web3.eth.getBalance(accountBalance);
-  if(displayBalance) displayBalance.innerHTML = `${web3.utils.fromWei(balance, 'ether')} ETH`;
+  console.log(balance);
+  return balance;
 }
 
+async function sendEthTransaction(to: string, from: string, amount: number): Promise<Transaction> {
+  const tranaction: Transaction = {
+    from: from,
+    to: to,
+    value: amount,
+    gas: await web3.eth.estimateGas()
+  }
 
-async function sendEthTransaction() {
   try {
     const blockAmount = Number(await web3.eth.getBlockNumber());
-    const sendTrx = await web3.eth.sendTransaction({
-      from: fromAddressInput ? fromAddressInput.value : '',
-      to: toAddressInput ? toAddressInput.value : '',
-      value: Number(web3.utils.toWei(valueInput ? valueInput.value: '', 'ether')),
-      gas: web3.eth.estimateGas()
-    });
+    const sendTrx = await web3.eth.sendTransaction(tranaction);
 
     if(displayBlocks) displayBlocks.innerHTML = `Amount of blocks when tranaction was made: ${blockAmount.toString()} `;
     
@@ -40,8 +54,22 @@ async function sendEthTransaction() {
   } catch(err) {
     console.log(err);
   }
+
+  console.log(tranaction);
+  return tranaction;
 }
 
+getBalanceBtn?.addEventListener('click', async () => {
+  const account: string | undefined = ethAddress?.value as string;
+  await getBalance(account);
+});
+
+sendTransactionBtn?.addEventListener('click', async () => {
+  const amount: number = Number(web3.utils.toWei(valueInput?.value, 'ether'));
+  const toAddress: string | undefined = toAddressInput?.value as string;
+  const fromAddress: string | undefined = fromAddressInput?.value as string;
+
+  await sendEthTransaction(toAddress, fromAddress, amount);
+});
+
 document.addEventListener('DOMContentLoaded', initApp);
-if(getBalanceBtn) getBalanceBtn.addEventListener('click', getBalance);
-if(sendTransactionBtn) sendTransactionBtn.addEventListener('click', sendEthTransaction);
